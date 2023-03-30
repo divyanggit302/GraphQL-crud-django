ae9cedc56b219eddee6f0ea56cf4727dad503e39
kishan savaliya <savaliyakishan9083@gmail.com>
2023-03-30 12:35:12 +0530

    test for home

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from mainapp.schema import schema
 from django.views.decorators.csrf import csrf_exempt
-
+from . import views
 
 urlpatterns = [
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path("",views.home ),
 ]  
 
